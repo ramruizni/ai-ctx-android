13acd81278ce3da01d6175003fad5a1c616f6a58
Rafael Ruiz <rafael@57blocks.com>
2025-08-29 18:32:01 -0500

    init poke14

---
+++ DeepSeekPokeAppFourteen/database/src/main/java/com/ramruizni/deepseekpokeappfourteen/database/DemoDatabase.kt
@@ -0,0 +1,18 @@
+package com.ramruizni.deepseekpokeappfourteen.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+//import com.ramruizni.deepseekpokeappfourteen.demo.datasource.daos.DemoDao
+//import com.ramruizni.deepseekpokeappfourteen.demo.datasource.dbdtos.DemoDbDto
+
+/*@Database(
+    entities = [
+        // NEW ENTITIES GO HERE
+        //DemoDbDto::class
+    ],
+    version = 1
+)
+abstract class DeepSeekPokeAppFourteenDatabase : RoomDatabase() {
+    // NEW DAOS GO HERE
+    //abstract fun demoDao(): DemoDao
+}*/
---
+++ DeepSeekPokeAppFourteen/navigation/src/main/java/com/ramruizni/deepseekpokeappfourteen/navigation/RootGraphRoute.kt
@@ -0,0 +1,6 @@
+package com.ramruizni.deepseekpokeappfourteen.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object RootGraphRoute
